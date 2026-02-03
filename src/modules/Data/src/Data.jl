53bb886239bfea8eef36dcd426a54b2cc3df88c2
Helisson Sousa <helissoncg@hotmail.com>
2026-02-03 15:19:07 -0300

    testMIP

--- src/modules/Data/src/Data.jl
+++ src/modules/Data/src/Data.jl
@@ -18,33 +18,28 @@ function read_instance(instance_file::String)
     instance_name = instance_file
     
     NM = data[1, 1]
-    println(NM)
-
+    
     NJ = data[1, 2]
-    println(NJ)
-
+    
     cap = Array{Int64}(undef, NM)
     for i in 1:NM
         cap[i] = data[2, i]
     end
-    println(cap)
-
+    
     cost = Array{Int64}(undef, NM, NJ)
     for i in 1:NM
         for j in 1:NJ
             cost[i,j] = data[i + 2, j]
         end
     end
-    println(cost)
-
+    
     w = Array{Int64}(undef, NM, NJ)
      for i in 1:NM
         for j in 1:NJ
             w[i,j] = data[i + 5, j]
         end
     end
-    println(w)
-    
+        
     instance_data = InstanceData(instance_name, NM, NJ, cap, cost, w)
 
     return instance_data
